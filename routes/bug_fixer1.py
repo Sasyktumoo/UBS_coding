a539d0b95a6d11ac7f7bd829384601298b1614a2
sasyktumoo <sasyktumoo@gmail.com>
2024-09-28 15:45:43 +0800

    opa

--- routes/bug_fixer1.py
+++ routes/bug_fixer1.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 import json
 import logging
 from flask import jsonify, request
@@ -6,44 +6,50 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/bugfixer/p1', methods=['POST'])
-def fixer():
-    stuff = request.get_json()
+def calculate_total_hours(durations, dependencies):
+    num_projects = len(durations)
     
-    class Thing():
-        def __init__(self):
-            self.before = []
-            self.val = None
+    project_graph = defaultdict(list)
+    dependency_count = [0] * num_projects
+    project_completion = [0] * num_projects
+    
+    for prerequisite, dependent in dependencies:
+        project_graph[prerequisite - 1].append(dependent - 1)
+        dependency_count[dependent - 1] += 1
+    
+    ready_queue = deque()
+    
+    for i in range(num_projects):
+        if dependency_count[i] == 0:
+            ready_queue.append(i)
+            project_completion[i] = durations[i]
+
+    while ready_queue:
+        current_project = ready_queue.popleft()
+
+        for dependent_project in project_graph[current_project]:
+            project_completion[dependent_project] = max(
+                project_completion[dependent_project], 
+                project_completion[current_project] + durations[dependent_project]
+            )
+            dependency_count[dependent_project] -= 1
             
-    def createGraph(vals, deps):
-        items = defaultdict(Thing)
-        x = len(vals)
-        
-        for i in range(x):
-            items[i].val = vals[i]
-        
-        for a, b in deps:
-            items[b - 1].before.append(items[a - 1])
-        
-        return items
+            if dependency_count[dependent_project] == 0:
+                ready_queue.append(dependent_project)
     
-    def calc(n):
-        if not n.before:
-            return n.val
-        if n.val is None:
-            n.val = max(calc(x) for x in n.before) + n.val
-        return n.val
+    return max(project_completion)
+
+@app.route('/bugfixer/p1', methods=['POST'])
+def project_time():
+    data = request.get_json()
     
-    output = []
+    results = []
     
-    for thing in stuff:
-        vals = thing['time']
-        deps = thing['prerequisites']
+    for project_info in data:
+        durations = project_info['time']
+        dependencies = project_info['prerequisites']
         
-        items = createGraph(vals, deps)
-        
-        max_val = max(calc(items[i]) for i in range(len(vals)))
-        
-        output.append(max_val)
+        total_hours = calculate_total_hours(durations, dependencies)
+        results.append(total_hours)
     
-    return jsonify(output)
+    return jsonify(results)
